d2c2f8e6f9f13ec4bc0ddcc9b8add5c3d245c8e2
ThiagoLima <tslyefad@gmail.com>
2025-12-21 11:36:00 -0400

    fix final amistos/IDsalvateste

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*", methods: ["GET", "POST"] },
-  pingTimeout: 10000,
-  pingInterval: 5000
+  pingTimeout: 20000,
+  pingInterval: 10000
 });
 
 // ===========================================================================
