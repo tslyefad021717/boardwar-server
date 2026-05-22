d5ddad2025b700fd111329e458a7fd9a2e008277
ThiagoLima <tslyefad@gmail.com>
2026-05-21 23:36:41 -0400

    fix final amistos/IDsalvaranked119

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ const STORE_CATALOG = {
 // ===========================================================================
 // 2. ESTADO GLOBAL
 // ===========================================================================
-const MIN_APP_VERSION = '1.4.9'; // Mude isso quando quiser forçar a atualização!
+const MIN_APP_VERSION = '1.5.0'; // Mude isso quando quiser forçar a atualização!
 
 function isVersionOutdated(clientVersion, minVersion) {
   const v1 = clientVersion.split('.').map(Number);
