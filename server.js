1ff2b27175c980c3b3c42fb08d934d058efbc791
ThiagoLima <tslyefad@gmail.com>
2026-05-29 01:08:54 -0400

    fix final amistos/IDsalvaranked132

--- server.js
+++ server.js
@@ -207,7 +207,7 @@ const STORE_CATALOG = {
 // 2. ESTADO GLOBAL
 // ===========================================================================
 const MIN_VERSION_ANDROID = "1.5.6";
-const MIN_VERSION_IOS = "1.5.2";
+const MIN_VERSION_IOS = "1.5.5";
 
 function isVersionOutdated(clientVersion, minVersion) {
   const v1 = clientVersion.split('.').map(Number);
