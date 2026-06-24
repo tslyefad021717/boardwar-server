4343e830895046b5d901a2fdd45a130445560612
ThiagoLima <tslyefad@gmail.com>
2026-06-24 08:59:28 -0400

    fix final amistos/IDsalvaranked143

--- server.js
+++ server.js
@@ -206,8 +206,8 @@ const STORE_CATALOG = {
 // ===========================================================================
 // 2. ESTADO GLOBAL
 // ===========================================================================
-const MIN_VERSION_ANDROID = "1.5.15";
-const MIN_VERSION_IOS = "1.5.15";
+const MIN_VERSION_ANDROID = "1.5.16";
+const MIN_VERSION_IOS = "1.5.16";
 
 function isVersionOutdated(clientVersion, minVersion) {
   const v1 = clientVersion.split('.').map(Number);
